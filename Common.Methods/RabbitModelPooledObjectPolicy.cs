405040769d8395f079fc00b379f3e6e1b716b36b
sbozhinov <sbojinov@yahoo.com>
2020-05-09 23:56:46 +0300

    Initial commit

---
+++ Common.Methods/RabbitModelPooledObjectPolicy.cs
@@ -0,0 +1,55 @@
+﻿using Common.Methods.Configurations;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Methods
+{
+    public class RabbitModelPooledObjectPolicy : IPooledObjectPolicy<IModel>
+    {
+        private readonly RabbitMqConfiguration _options;
+
+        public RabbitModelPooledObjectPolicy(IOptions<RabbitMqConfiguration> rabbitConf)
+        {
+            this._options = rabbitConf.Value;
+            this.Connection = this.GetConnection();
+        }
+
+        public IConnection Connection { get; private set; }
+
+        private IConnection GetConnection()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _options.HostName,
+                UserName = _options.UserName,
+                Password = _options.Password,
+                Port = _options.Port,
+                VirtualHost = _options.VHost
+            };
+
+            return factory.CreateConnection();
+        }
+
+        public IModel Create()
+        {
+            return this.Connection.CreateModel();
+        }
+
+        public bool Return(IModel obj)
+        {
+            if (obj.IsOpen)
+            {
+                return true;
+            }
+            else
+            {
+                obj?.Dispose();
+                return false;
+            }
+        }
+    }
+}
---
+++ Common.Methods/RabbitServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Common.Methods;
+using Common.Methods.Configurations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.ObjectPool;
+using RabbitMQ.Client;
+using System;
+
+namespace Common.RabbitMq
+{
+    public static class RabbitServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRabbit(this IServiceCollection services, IConfiguration configuration)
+        {
+            var rabbitConfig = configuration.GetSection("RabbitConfiguration");
+            services.Configure<RabbitMqConfiguration>(rabbitConfig);
+
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+            services.AddSingleton<IPooledObjectPolicy<IModel>, RabbitModelPooledObjectPolicy>();
+
+            return services;
+        }
+    }
+}
